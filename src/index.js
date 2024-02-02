664d2b6f4e59e3df5b8cb8365b49d16f36ce6cb0
happex <110610727+okayhappex@users.noreply.github.com>
2024-02-03 00:16:34 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// Mise en place des env
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Mise en place de l'application express
+const express = require('express');
+const app = express();
+const port = 5000;
+
+// Init. de la database
+const { Deta } = require('deta');
+const deta = Deta(process.env.DATA_KEY);
+const links = deta.Base('links');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/new', async (req, res) => {
+    let article = await links.get(req.query.id);
+
+    if (article) {
+        res.status(409).json(article);
+    } else if (req.query.link) {
+        await links.put(data = { "link": req.query.link }, key = req.query.id);
+        res.status(200).json("Success");
+    } else {
+        res.status(400).json("Bad Request");
+    }
+}); // Libre pour l'instant, peut-être réservée aux personnes détenant un compte Scratch dans le futur
+
+app.get('/:id', async (req, res) => {
+    let article = await links.get(req.params.id);
+
+    if (article) {
+        res.redirect(`https://fr.scratch-wiki.info/wiki/${article.link}`);
+    } else {
+        res.status(404).json("Not Found");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
